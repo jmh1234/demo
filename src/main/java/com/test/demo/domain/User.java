6aec992abcfc70c54edbe4e68d230a680512dd9b
JJB <923268677@qq.com>
2020-07-22 10:23:04 +0800

    数据库信息修改

--- src/main/java/com/test/demo/domain/User.java
+++ src/main/java/com/test/demo/domain/User.java
@@ -3,11 +3,19 @@ package com.test.demo.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.sql.Timestamp;
+
 @Setter
 @Getter
 public class User {
-    private Integer id;
-    private String userName;
+    private int id;
+    private String tel;
+    private String name;
+    private String address;
+    private Timestamp createTime;
+    private Timestamp updateTime;
+    private int status;
+    private String username;
     private String password;
-    private Integer age;
+    private int age;
 }
