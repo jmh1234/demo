7933fd8400638c0adb46a29a66ff08ba7d7e4f84
JJB <923268677@qq.com>
2020-07-27 17:05:48 +0800

    reset

--- src/main/java/com/test/demo/Test.java
+++ src/main/java/com/test/demo/Test.java
@@ -3,9 +3,5 @@ package com.test.demo;
 public class Test {
     public static void main(String[] args) {
         System.out.println("哈哈哈");
-        for (int i = 0; i < 10; i++) {
-            if (i % 2 == 0) System.out.println("这是一个偶数");
-            else System.out.println("则是一个奇数");
-        }
     }
 }
