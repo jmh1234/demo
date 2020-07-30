65295e3a0f0b70f58485e78bf087e64216d7c36e
JJB <923268677@qq.com>
2020-07-30 13:57:32 +0800

    fileChange

--- src/main/java/com/test/demo/Test.java
+++ src/main/java/com/test/demo/Test.java
@@ -1,7 +1,18 @@
 package com.test.demo;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("哈哈哈");
+        List<Integer> list = Arrays.asList(49, 0, 50);
+        list.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return "123".indexOf(o1, o2);
+            }
+        });
+        list.forEach(System.out::println);
     }
 }
