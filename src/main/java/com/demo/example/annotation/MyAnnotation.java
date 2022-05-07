9b89bd351ad77d51a8e8460b9c47220da8c005ec
jimh <jimh@jimhdeMacBook-Pro.local>
2022-05-07 11:40:35 +0800

    问题修改

--- src/main/java/com/demo/example/annotation/MyAnnotation.java
+++ src/main/java/com/demo/example/annotation/MyAnnotation.java
@@ -4,6 +4,13 @@ import org.springframework.context.annotation.Scope;
 
 import java.lang.annotation.*;
 
+/**
+ * Created with IntelliJ IDEA.
+ * 我的自定义注解
+ *
+ * @author Ji MingHao
+ * @since 2022-04-08 11:35
+ */
 @Documented
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
